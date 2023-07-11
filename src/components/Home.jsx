d0f2768d6b59ca1ad6b5aac6ab580e950af1a0a0
guille1999 <90706729+guille1999utp@users.noreply.github.com>
2023-07-11 01:32:54 -0500

    agregando estructuracion de diseño y librerias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div class="container">
+      <div class="products">
+        <h2 class="subtitle">
+          Store
+        </h2>
+        <hr></hr>
+      </div>
+      <div class="selected-product">
+        <h2 class="subtitle">
+          Producto
+        </h2>
+        <hr></hr>
+      </div>
+    </div>
   )
 }
 
